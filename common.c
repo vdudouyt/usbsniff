fbe8ec1fd1b8e6d732e9b597944f03814c3aba91
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2014-04-18 09:40:37 +0700

    hex_to_int fix

--- common.c
+++ common.c
@@ -26,6 +26,6 @@ int hex_to_buf(const char *str, unsigned char *buf) {
 
 unsigned int hex_to_int(const char *str) {
 	unsigned int result;
-	sscanf(str, "%ux", &result);
+	sscanf(str, "%x", &result);
 	return(result);
 }
