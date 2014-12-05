1313654e27d2b3614cb205472ff16f1ff4e64409
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2014-12-05 18:13:34 +0700

    Added license in the source files

--- lib/USBSniff/Parser.pm
+++ lib/USBSniff/Parser.pm
@@ -1,7 +1,20 @@
-package USBSniff::Parser;
-use strict;
-
 =pod
+--------------------------------------------------------------------
+(c) Valentin Dudouyt, 2013 - 2014                                                                                          
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------
+				     
 A Perl binding for dealing with the Human-Readable Format.
 Synopsis:
   my @packets = USBSniff::Parser::parse_file($ARGV[0]);
@@ -10,6 +23,9 @@ Synopsis:
   }
   USBSniff::Parser::dump(@packets);
 =cut
+
+package USBSniff::Parser;
+use strict;
 
 sub parse {
 	my ($data) = @_;
